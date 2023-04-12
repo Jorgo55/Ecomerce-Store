045b091706721a557c25eb3c5d55d72aa3792d57
jbardho <123744837+jbardho@users.noreply.github.com>
2023-04-12 23:38:24 +0200

    seller authenticaton

---
+++ src/app/auth.guard.ts
@@ -0,0 +1,28 @@
+import { SellerService } from 'src/app/services/seller.service';
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuard implements CanActivate {
+  constructor(private sellerService: SellerService) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    return this.sellerService.isSellerLoggedIn;
+  }
+  // reloadSeller()
+}
