87a6fcfb64782c26c18acc18d9cb239baf3b94b8
jbardho <123744837+jbardho@users.noreply.github.com>
2023-04-19 23:06:18 +0200

    seller home login updated

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  menuType: String = 'default';
+  constructor(private router: Router) {}
 
+  ngOnInit(): void {
+    this.router.events.subscribe((val: any) => {
+      // console.log(val.url);
+      if (val.url) {
+        if (localStorage.getItem('seller') && val.url.includes('seller')) {
+          this.menuType = 'seller';
+          console.log('in seler area');
+        } else {
+          console.log('outisde seller');
+          this.menuType = 'default';
+        }
+      }
+    });
+  }
 }
