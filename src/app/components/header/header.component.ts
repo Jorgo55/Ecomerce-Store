b761dca4c882fc704c4a015fdf5a115a8a37ac4a
jbardho <123744837+jbardho@users.noreply.github.com>
2023-04-22 03:51:08 +0200

    small fix

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,23 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
-  menuType: String = 'default';
+  menuType: string = 'default';
+
   constructor(private router: Router) {}
 
   ngOnInit(): void {
     this.router.events.subscribe((val: any) => {
-      // console.log(val.url);
       if (val.url) {
         if (localStorage.getItem('seller') && val.url.includes('seller')) {
           this.menuType = 'seller';
-          console.log('in seler area');
+          console.log('in seller area');
         } else {
-          console.log('outisde seller');
+          console.log('outside seller');
           this.menuType = 'default';
         }
       }
